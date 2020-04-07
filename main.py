69cc3c8e84ccfad5bf7bdcdbb52c0dcc88f920b9
Edgard Diaz <50925001+josgard94@users.noreply.github.com>
2020-04-07 16:51:36 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+"""
+Author: Edgard Diaz
+Date: April  07th  2020
+
+Dowload videos from YouTube with this application developed with python 3.
+
+These modules need to be installed:
+
+    pip installa pytube3
+		pip install pyinstaller 
+
+To compile the project use
+		pyinstaller  --clean --win-private-assemblies --noupx --onedir --onefile main.py
+
+"""
+
+from pytube import YouTube
+import keyboard
+
+url = input('Url: ')
+
+folder = input('Save download to: ')
+
+video = YouTube(url)
+
+print(video.title)
+
+stream = video.streams.first()
+
+stream.download(folder)
+
+print('Video descargado')
+
+print('Presione la tecla esc para terminar.')
+while True:
+
+    if keyboard.is_pressed('esc'):
+    	exit()
+
+exit()
